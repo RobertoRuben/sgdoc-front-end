03c53672bca7a75dbc91c587471c44e3a1b610de
RobertoRuben <robertoch263@gmail.com>
2025-02-25 10:40:16 -0500

    ✨ feat: formatear el ID en la tabla de trabajadores para que tenga un tamaño fijo de 5 caracteres

--- src/pages/trabajadores-page/components/TrabajadorTable.tsx
+++ src/pages/trabajadores-page/components/TrabajadorTable.tsx
@@ -96,7 +96,7 @@ export const TrabajadorTable: React.FC<TrabajadorTableProps> = ({trabajadores, d
                                     } hover:bg-gray-100 transition-colors duration-150 ease-in-out`}
                                 >
                                     <TableCell className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                        {trabajador.id}
+                                        {String(trabajador.id).padStart(5, "0")}
                                     </TableCell>
                                     <TableCell className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
                                         {trabajador.dni}
